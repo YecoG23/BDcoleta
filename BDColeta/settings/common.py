22e7647b852ac33932e198e739edf0a2c369f808
Yeco <oscco.yoshin@gmail.com>
2018-02-22 20:42:48 -0300

    get rid of storagewhitenoise

--- BDColeta/settings/common.py
+++ BDColeta/settings/common.py
@@ -140,7 +140,7 @@ STATIC_ROOT = BASE_DIR + '/static'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, '/peixes/static_own')]
