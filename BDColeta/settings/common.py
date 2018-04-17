911888805a480b25943c7cccc802ad1470da9977
Yeco <oscco.yoshin@gmail.com>
2018-04-17 13:53:29 -0300

    fix email server

--- BDColeta/settings/common.py
+++ BDColeta/settings/common.py
@@ -157,7 +157,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #SENDGRID CONFIG
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'oscco.yoshin@gmail.com'
+EMAIL_HOST_USER = 'yosh23'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'bdcoleta@gmail.com'
