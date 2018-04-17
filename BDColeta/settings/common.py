3b90ff3919af87d48038eebb07bc8e346873c1e1
Yeco <oscco.yoshin@gmail.com>
2018-04-17 11:47:27 -0300

    fix it sengrid password

--- BDColeta/settings/common.py
+++ BDColeta/settings/common.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yosh23'
-EMAIL_HOST_PASSWORD = 'yecoSendgrid23'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'BDColeta Admin'
 
